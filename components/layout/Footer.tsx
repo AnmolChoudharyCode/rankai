c53903e5c358af6f579648d9dd764f6a6fb6d90b
Anmol Choudhary <AnmolC@jmf.com>
2026-01-29 14:02:51 +0530

    Testing-2

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -27,6 +27,7 @@ export default function Footer() {
   };
 
   //Added the comment for testing the code review process
+  //Added the comment for testing the code review process - push strategy
 
   return (
     <footer className="bg-white border-t border-gray-200 mt-auto">
